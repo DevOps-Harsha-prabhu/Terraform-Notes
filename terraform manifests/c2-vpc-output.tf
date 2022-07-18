03fbded074c5c0c81aca156400d10077a73a5988
devops-test-user <nani.ud.ac1@gmail.com>
2022-07-18 09:51:38 +0530

    1st commit

---
+++ terraform manifests/c2-vpc-output.tf
@@ -0,0 +1,8 @@
+output "vpc_id1" {
+    value = "${aws_vpc.vpc1.id}"
+  
+}
+output "vpc_arn1" {
+    value = "${aws_vpc.vpc1.arn}"
+  
+}
---
+++ terraform manifests/c2-vpc.tf
@@ -0,0 +1,8 @@
+resource "aws_vpc" "vpc1" {
+  cidr_block           = "10.0.0.0/16"
+  enable_dns_hostnames = true
+  tags = {
+    Name = "vpc1"
+    Env  = "production"
+  }
+}
